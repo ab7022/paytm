fe9c520dc7961d22beffe3f548c494b9c95dcf1e
ab7022 <bayees1@gmail.com>
2024-02-03 15:49:11 +0530

    added vercel file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Router>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/sendMoney" element={<SendMoney />} />
