a9a53dd171c6c3f237dfce138595897ed69647d0
ab7022 <bayees1@gmail.com>
2024-02-16 02:38:12 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -38,5 +38,5 @@ app.use((err, req, res, next) => {
 
 
 app.listen(4000, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port`);
 });
--- backend/middleware.js
+++ backend/middleware.js
@@ -2,21 +2,22 @@ const JWT_SECRET = require("./config");
 const jwt = require("jsonwebtoken");
 
 function authMiddleware(req,res,next){
-    const authHeader = req.headers.authorization
+    const authHeader = req.headers.authorization 
     if(!authHeader){
         return res.status(403).json({
             msg:"Token Not Found"
         });
 
     }
-    const token = authHeader[0];
+    const token = authHeader;
     console.log(token);
     try {
         const decoded = jwt.verify(token,JWT_SECRET)
         req.userId = decoded.userId.toString(); 
     } catch (error) {
+        console.log(error);
         return res.status(403).json({
-            msg:"Something Wen Wrong"
+            msg:"Something Went Wrong"
         });
 
     }
