214b81e7e68a0278e0ac0673df834ac04f545eee
ab7022 <bayees1@gmail.com>
2024-02-16 02:29:48 +0530

    test

--- backend/middleware.js
+++ backend/middleware.js
@@ -3,13 +3,14 @@ const jwt = require("jsonwebtoken");
 
 function authMiddleware(req,res,next){
     const authHeader = req.headers.authorization
-    if(!authHeader || !authHeader.startsWith("Bearer ")){
+    if(!authHeader){
         return res.status(403).json({
             msg:"Token Not Found"
         });
 
     }
-    const token = authHeader.split(' ')[1];
+    const token = authHeader[0];
+    console.log(token);
     try {
         const decoded = jwt.verify(token,JWT_SECRET)
         req.userId = decoded.userId.toString(); 
