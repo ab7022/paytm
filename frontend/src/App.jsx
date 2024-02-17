5516ddc095dbc67b8a9faee110d5fff4716918b1
ab7022 <bayees1@gmail.com>
2024-02-18 04:55:15 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 
 import Signup from './pages/Signup';
