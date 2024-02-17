f60589b49118563cb91543dc9d88b5eddffe345e
ab7022 <bayees1@gmail.com>
2024-02-17 18:35:18 +0530

    added camera option

--- frontend/src/components/QrScanner.jsx
+++ frontend/src/components/QrScanner.jsx
@@ -1,6 +1,7 @@
 // QRCodeScanner.jsx
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import QrScanner from "react-qr-scanner";
+
 export const QRCodeScanner = ({ onScan }) => {
   const [qrCodeData, setQrCodeData] = useState(null);
 
@@ -14,16 +15,12 @@ export const QRCodeScanner = ({ onScan }) => {
   const handleError = (error) => {
     console.error(error);
   };
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then(devices => {
-      console.log(devices);
-    });
-  }, []);
+
   return (
     <QrScanner
       onScan={handleScan}
       onError={handleError}
-      constraints={{ video: { facingMode: "environment" } }}
+      constraints={{ video: true, facingMode: "environment" }}
       className="w-full max-h-64 border border-gray-300 rounded-xl shadow overflow-hidden"
     />
   );
